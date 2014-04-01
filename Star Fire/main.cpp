f0e336a895b8d24b20c9fbc6c31fd139d909bc59
Eric Boisvert <epboisvert@gmail.com>
2014-04-01 11:19:01 -0400

    Initial Commit

---
+++ Star Fire/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Star Fire
+//
+//  Created by La Li Lu Le Lo on 2014-04-01.
+//  Copyright (c) 2014 Eric Boisvert. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
